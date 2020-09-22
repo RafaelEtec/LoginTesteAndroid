c3f5524876b4d6cad26281594dd0a4941b61f726
Rafael Goulart <Rafael.Goulart2@etecia.com.br>
2020-09-21 22:35:48 -0300

    2nd Commit Adicionados:  - Sobre Facebook

---
+++ app/src/main/java/br/com/local/appcomponentes2h/Facebook_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.appcomponentes2h;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Facebook_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_layout);
+    }
+}
