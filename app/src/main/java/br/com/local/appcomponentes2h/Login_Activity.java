3efdeb8275fc4198dec0fceb34de666b03059334
Rafael Goulart <Rafael.Goulart2@etecia.com.br>
2020-10-14 19:31:17 -0300

    5th Commit Adicionados:  - Splash -> Login (ir automático, 3s)

--- app/src/main/java/br/com/local/appcomponentes2h/Login_Activity.java
+++ app/src/main/java/br/com/local/appcomponentes2h/Login_Activity.java
@@ -31,6 +31,7 @@ public class Login_Activity extends AppCompatActivity {
 
                 if (email.equals("Rafael@gmail.com") && senha.equals("etecia")) {
                     startActivity(new Intent(getApplicationContext(), MenuPrincipal_Activity.class));
+                    txtEmailUsu.requestFocus();
                 }else {
                     Toast.makeText(getApplicationContext(),"Usuário ou senha Inválidos!!!",Toast.LENGTH_SHORT).show();
                 }
--- app/src/main/java/br/com/local/appcomponentes2h/SplashActivity.java
+++ app/src/main/java/br/com/local/appcomponentes2h/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -33,5 +34,13 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Login_Activity.class));
             }
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                        Login_Activity.class));
+            }
+        }, 3000);
     }
 }
